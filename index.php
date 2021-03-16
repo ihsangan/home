b2dae60269537467763f7e575175e7a9afae90c0
Ihsan <me@iganss.xyz>
2021-03-17 06:28:05 +0800

    Add google search bar

--- index.php
+++ index.php
@@ -9,30 +9,20 @@ include'style.php';
 <meta content='IE=edge,chrome=1' http-equiv='X-UA-Compatible'/>
 <title>Search Engine</title>
 <style>
-@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css');
 *{
-  margin: 3px;
-  padding: 0;
-  outline: none;
-  box-sizing: border-box;
-  font-family: sans-serif;
+margin: 3px;
+padding: 0;
+outline: none;
+box-sizing: border-box;
+font-family: sans-serif;
 }
 html,body{background : #FAFAFA}
-b { font-weight: bold; }
-table { border: 1px solid gray;}
-td { text-align: center; padding: 25;}
 .giff{width : 495px; height : 435px;  background-size : 100% 100%;}
-form{width : 100%;}
-form input:nth-child(1){padding : 10px; border-radius : 50px; border : 2px solid #ccc; width : 520px;}
-form label {background : <?php echo $colour;?>; border : none; padding : 10px; border-radius : 50%; color : #fff;}
-pg{float : right; margin-right : 5px;}
-a {text-decoration : none; color : #FF5500;}
 @media(max-width : 550px){
 .giff{width : 90%;}
 form input:nth-child(1){width : 78%;}
 }
 @media(max-width : 480px){
-    
 .giff{height : 390px}
 }
 @media(max-width : 440px){
@@ -42,23 +32,16 @@ form input:nth-child(1){width : 78%;}
 .giff{height : 340px;}
 }
 </style>
+<script async src="https://cse.google.com/cse.js?cx=96d15ab0b39f339c4"></script>
 </head>
 <body>
 <center>
 <div class="giff" style="background-image : url('https://cdn.jsdelivr.net/gh/ihsangan/files/1.webp')"></div>
 <br>
-<form method="GET" action="https://google.com/search"/>
-<input type="search" name="q" required/>
-<input type="submit" id="src" style="display: none" value=""/>
-<label for="src"><i class="fa fa-search"></i></label>
-</form>
+<div class="gcse-search"></div>
 </center><br>
 <script>
-$('img').mousedown(function (e) {
-  if(e.button == 2) {
-    return false;
-  }
-}
+$('img').mousedown(function (e) { if(e.button == 2) { return false; }}
 </script>
 </body>
 </html>
